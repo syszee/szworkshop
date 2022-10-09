94d0325f43eed9d4e49fd50900d8031f3f9818b7
AutumnAurelium <AutumnAurelium@users.noreply.github.com>
2022-10-09 14:06:15 -0400

    Cleanup and organization pass

--- src/main/java/me/aurelium/szworkshop/entity/QuiverEntity.java
+++ src/main/java/me/aurelium/szworkshop/entity/QuiverEntity.java
@@ -1,10 +1,16 @@
 package me.aurelium.szworkshop.entity;
 
 import me.aurelium.szworkshop.SZWorkshop;
-import net.minecraft.entity.*;
+import net.minecraft.entity.EntityDimensions;
+import net.minecraft.entity.EntityPose;
+import net.minecraft.entity.EntityType;
+import net.minecraft.entity.LivingEntity;
 import net.minecraft.entity.ai.RangedAttackMob;
 import net.minecraft.entity.ai.goal.*;
-import net.minecraft.entity.ai.pathing.*;
+import net.minecraft.entity.ai.pathing.BirdNavigation;
+import net.minecraft.entity.ai.pathing.EntityNavigation;
+import net.minecraft.entity.ai.pathing.MobNavigation;
+import net.minecraft.entity.ai.pathing.PathNodeType;
 import net.minecraft.entity.attribute.DefaultAttributeContainer;
 import net.minecraft.entity.attribute.EntityAttributes;
 import net.minecraft.entity.damage.DamageSource;
@@ -14,7 +20,8 @@ import net.minecraft.entity.data.TrackedDataHandlerRegistry;
 import net.minecraft.entity.mob.CreeperEntity;
 import net.minecraft.entity.mob.MobEntity;
 import net.minecraft.entity.mob.Monster;
-import net.minecraft.entity.passive.*;
+import net.minecraft.entity.passive.PassiveEntity;
+import net.minecraft.entity.passive.TameableEntity;
 import net.minecraft.entity.player.PlayerEntity;
 import net.minecraft.entity.player.PlayerInventory;
 import net.minecraft.entity.projectile.PersistentProjectileEntity;
@@ -25,7 +32,6 @@ import net.minecraft.item.ItemStack;
 import net.minecraft.nbt.NbtCompound;
 import net.minecraft.nbt.NbtElement;
 import net.minecraft.nbt.NbtList;
-import net.minecraft.particle.ParticleEffect;
 import net.minecraft.particle.ParticleTypes;
 import net.minecraft.screen.Generic3x3ContainerScreenHandler;
 import net.minecraft.screen.NamedScreenHandlerFactory;
@@ -39,7 +45,6 @@ import net.minecraft.util.math.MathHelper;
 import net.minecraft.util.math.Vec3d;
 import net.minecraft.util.random.RandomGenerator;
 import net.minecraft.world.World;
-import net.minecraft.world.WorldView;
 import org.jetbrains.annotations.Nullable;
 import software.bernie.geckolib3.core.IAnimatable;
 import software.bernie.geckolib3.core.PlayState;
@@ -62,10 +67,10 @@ public class QuiverEntity extends TameableEntity implements IAnimatable, RangedA
 
 	protected QuiverEntity(EntityType<QuiverEntity> entityType, World world) {
 		super(entityType, world);
-		inventory.addListener(inv -> {
-			this.dataTracker.set(ARROWS, countArrows());
-		});
+		inventory.addListener(inv -> this.dataTracker.set(ARROWS, countArrows()));
 	}
+
+	// Behavior methods
 
 	@Override
 	protected void mobTick() {
@@ -73,37 +78,6 @@ public class QuiverEntity extends TameableEntity implements IAnimatable, RangedA
 		if(!world.getServer().getGameRules().getBoolean(SZWorkshop.quiverRule)) {
 			this.kill();
 		}
-	}
-
-	@Override
-	protected void initGoals() {
-		this.goalSelector.add(1, new SwimGoal(this));
-		this.goalSelector.add(2, new ProjectileAttackNoLookGoal(this, 1.25, 2, 20, 10.0F));
-		this.goalSelector.add(3, new FollowOwnerNoTeleportGoal(this, 1f, 5f, 2f, false));
-		this.goalSelector.add(10, new LookAtEntityGoal(this, PlayerEntity.class, 8.0F));
-
-		this.targetSelector.add(1, new TrackOwnerAttackerGoal(this));
-		this.targetSelector.add(2, new RevengeGoal(this));
-		this.targetSelector.add(3, new AttackWithOwnerGoal(this));
-		this.targetSelector
-				.add(3, new TargetGoal(this, MobEntity.class, 5, false, false, entity -> entity instanceof Monster && !(entity instanceof CreeperEntity)));
-	}
-
-	@Override
-	protected float getActiveEyeHeight(EntityPose pose, EntityDimensions dimensions) {
-		return 0.85f;
-	}
-
-	public static DefaultAttributeContainer.Builder createMobAttributes() {
-		return MobEntity.createMobAttributes()
-				.add(EntityAttributes.GENERIC_MAX_HEALTH, 50f)
-				.add(EntityAttributes.GENERIC_MOVEMENT_SPEED, 0.2f);
-	}
-
-	@Override
-	protected void initDataTracker() {
-		super.initDataTracker();
-		this.dataTracker.startTracking(ARROWS, 0);
 	}
 
 	@Override
@@ -125,8 +99,8 @@ public class QuiverEntity extends TameableEntity implements IAnimatable, RangedA
 
 				arrow.setPos(g, h, j);
 				arrow.setVelocity(
-						world.random.nextTriangular(0.0, 0.11485000171139836),
-						world.random.nextTriangular(0.2, 0.11485000171139836),
+						world.random.nextTriangular(0.0, 0.11485000171139836), // wish I knew what caused the decompiler to generate these weird constants
+						world.random.nextTriangular(0.2, 0.11485000171139836), // but I'm not touching them
 						world.random.nextTriangular(0.0, 0.11485000171139836)
 				);
 				world.spawnEntity(arrow);
@@ -136,9 +110,67 @@ public class QuiverEntity extends TameableEntity implements IAnimatable, RangedA
 	}
 
 	@Override
-	protected void dropInventory() {
+	public ActionResult interactAt(PlayerEntity player, Vec3d hitPos, Hand hand) {
+		if(!isTamed())
+			setOwner(player);
 
+		if(!player.world.isClient) {
+			player.openHandledScreen(this);
+		}
+
+		return ActionResult.success(true);
 	}
+
+	@Override
+	public void attack(LivingEntity target, float pullProgress) {
+		PersistentProjectileEntity arrow = createRandomArrow(pullProgress);
+
+		if(arrow != null) {
+			double d = target.getX() - this.getX();
+			double e = target.getBodyY(0.1f) - arrow.getY();
+			double f = target.getZ() - this.getZ();
+			double g = Math.sqrt(d * d + f * f);
+			arrow.setPos(this.getX() + d * 0.1, this.getY() + 0.9f, this.getZ() + f * 0.1);
+			arrow.setVelocity(d, e + g * 0.2F, f, 3F, 0);
+			this.playSound(SoundEvents.ENTITY_SKELETON_SHOOT, 1.0F, 1.0F / (this.getRandom().nextFloat() * 0.4F + 0.8F));
+			this.world.spawnEntity(arrow);
+		}
+	}
+
+	@Override
+	protected float getActiveEyeHeight(EntityPose pose, EntityDimensions dimensions) {
+		return 0.85f;
+	}
+
+	// Data-tracking and AI methods
+
+	@Override
+	protected void initGoals() {
+		this.goalSelector.add(1, new SwimGoal(this));
+		this.goalSelector.add(2, new ProjectileAttackNoLookGoal(this, 1.25, 2, 20, 10.0F));
+		this.goalSelector.add(3, new FollowOwnerNoTeleportGoal(this, 1f, 5f, 2f));
+		this.goalSelector.add(10, new LookAtEntityGoal(this, PlayerEntity.class, 8.0F));
+
+		this.targetSelector.add(1, new TrackOwnerAttackerGoal(this));
+		this.targetSelector.add(2, new RevengeGoal(this));
+		this.targetSelector.add(3, new AttackWithOwnerGoal(this));
+		this.targetSelector
+				.add(3, new TargetGoal<>(this, MobEntity.class, 5, false, false, entity -> entity instanceof Monster && !(entity instanceof CreeperEntity)));
+	}
+
+	public static DefaultAttributeContainer.Builder createMobAttributes() {
+		return MobEntity.createMobAttributes()
+				.add(EntityAttributes.GENERIC_MAX_HEALTH, 50f)
+				.add(EntityAttributes.GENERIC_MOVEMENT_SPEED, 0.2f);
+	}
+
+	@Override
+	protected void initDataTracker() {
+		super.initDataTracker();
+		this.dataTracker.startTracking(ARROWS, 0);
+	}
+
+	// Inventory-related methods
 
 	private int countArrows() {
 		int count = 0;
@@ -163,19 +195,28 @@ public class QuiverEntity extends TameableEntity implements IAnimatable, RangedA
 		return i;
 	}
 
-	@Override
-	public ActionResult interactAt(PlayerEntity player, Vec3d hitPos, Hand hand) {
-		if(!isTamed())
-			setOwner(player);
+	@Nullable
+	private PersistentProjectileEntity createRandomArrow(float damageModifier) {
+		int randomArrowItem = chooseSlotWithArrow(world.random);
 
-		if(!player.world.isClient) {
-			player.openHandledScreen(this);
+		if(randomArrowItem != -1) {
+			ItemStack itemStack = inventory.getStack(randomArrowItem);
+
+			inventory.removeStack(randomArrowItem, 1);
+
+			return ProjectileUtil.createArrowProjectile(this, itemStack, damageModifier);
 		}
 
-		return ActionResult.success(true);
+		return null;
 	}
 
+	@Nullable
+	@Override
+	public ScreenHandler createMenu(int i, PlayerInventory playerInventory, PlayerEntity playerEntity) {
+		return new Generic3x3ContainerScreenHandler(i, playerInventory, inventory);
+	}
 
+	// NBT Serialization methods
 	// Copied from the Donkey, oddly.
 	@Override
 	public void writeCustomDataToNbt(NbtCompound nbt) {
@@ -208,6 +249,8 @@ public class QuiverEntity extends TameableEntity implements IAnimatable, RangedA
 		}
 		super.readCustomDataFromNbt(nbt);
 	}
+
+	// Animation methods
 
 	private <E extends IAnimatable> PlayState predicate(AnimationEvent<E> event) {
 
@@ -252,70 +295,30 @@ public class QuiverEntity extends TameableEntity implements IAnimatable, RangedA
 		return factory;
 	}
 
+	// Unfortunately we need this to take advantage of tameable mob code.
 	@Nullable
 	@Override
 	public PassiveEntity createChild(ServerWorld world, PassiveEntity entity) {
 		return new QuiverEntity(SZEntities.QUIVER, world);
 	}
 
-	@Nullable
-	private PersistentProjectileEntity createRandomArrow(float damageModifier) {
-		int randomArrowItem = chooseSlotWithArrow(world.random);
-
-		if(randomArrowItem != -1) {
-			ItemStack itemStack = inventory.getStack(randomArrowItem);
-
-			inventory.removeStack(randomArrowItem, 1);
-
-			return ProjectileUtil.createArrowProjectile(this, itemStack, damageModifier);
-		}
-
-		return null;
-	}
-
-	@Override
-	public void attack(LivingEntity target, float pullProgress) {
-		PersistentProjectileEntity arrow = createRandomArrow(pullProgress);
-
-		if(arrow != null) {
-			double d = target.getX() - this.getX();
-			double e = target.getBodyY(0.1f) - arrow.getY();
-			double f = target.getZ() - this.getZ();
-			double g = Math.sqrt(d * d + f * f);
-			arrow.setPos(this.getX() + d * 0.1, this.getY() + 0.9f, this.getZ() + f * 0.1);
-			arrow.setVelocity(d, e + g * 0.2F, f, 3F, 0);
-			this.playSound(SoundEvents.ENTITY_SKELETON_SHOOT, 1.0F, 1.0F / (this.getRandom().nextFloat() * 0.4F + 0.8F));
-			this.world.spawnEntity(arrow);
-		}
-	}
-
-	@Nullable
-	@Override
-	public ScreenHandler createMenu(int i, PlayerInventory playerInventory, PlayerEntity playerEntity) {
-		return new Generic3x3ContainerScreenHandler(i, playerInventory, inventory);
-	}
-
 	// This is just the same as FollowOwnerGoal but with the teleportation code removed.
 	private static class FollowOwnerNoTeleportGoal extends Goal {
 		private final TameableEntity tameable;
 		private LivingEntity owner;
-		private final WorldView world;
 		private final double speed;
 		private final EntityNavigation navigation;
 		private int updateCountdownTicks;
 		private final float maxDistance;
 		private final float minDistance;
 		private float oldWaterPathfindingPenalty;
-		private final boolean leavesAllowed;
 
-		public FollowOwnerNoTeleportGoal(TameableEntity tameable, double speed, float minDistance, float maxDistance, boolean leavesAllowed) {
+		public FollowOwnerNoTeleportGoal(TameableEntity tameable, double speed, float minDistance, float maxDistance) {
 			this.tameable = tameable;
-			this.world = tameable.world;
 			this.speed = speed;
 			this.navigation = tameable.getNavigation();
 			this.minDistance = minDistance;
 			this.maxDistance = maxDistance;
-			this.leavesAllowed = leavesAllowed;
 			this.setControls(EnumSet.of(Goal.Control.MOVE, Goal.Control.LOOK));
 			if (!(tameable.getNavigation() instanceof MobNavigation) && !(tameable.getNavigation() instanceof BirdNavigation)) {
 				throw new IllegalArgumentException("Unsupported mob type for FollowOwnerGoal");
@@ -390,10 +393,6 @@ public class QuiverEntity extends TameableEntity implements IAnimatable, RangedA
 		private final float maxShootRange;
 		private final float squaredMaxShootRange;
 
-		public ProjectileAttackNoLookGoal(RangedAttackMob mob, double mobSpeed, int intervalTicks, float maxShootRange) {
-			this(mob, mobSpeed, intervalTicks, intervalTicks, maxShootRange);
-		}
-
 		public ProjectileAttackNoLookGoal(RangedAttackMob mob, double mobSpeed, int minIntervalTicks, int maxIntervalTicks, float maxShootRange) {
 			if (!(mob instanceof LivingEntity)) {
 				throw new IllegalArgumentException("ArrowAttackGoal requires Mob implements RangedAttackMob");
@@ -465,7 +464,7 @@ public class QuiverEntity extends TameableEntity implements IAnimatable, RangedA
 				this.updateCountdownTicks = MathHelper.floor(f * (float)(this.maxIntervalTicks - this.minIntervalTicks) + (float)this.minIntervalTicks);
 			} else if (this.updateCountdownTicks < 0) {
 				this.updateCountdownTicks = MathHelper.floor(
-						MathHelper.lerp(Math.sqrt(d) / (double)this.maxShootRange, (double)this.minIntervalTicks, (double)this.maxIntervalTicks)
+						MathHelper.lerp(Math.sqrt(d) / (double)this.maxShootRange, this.minIntervalTicks, this.maxIntervalTicks)
 				);
 			}
 
