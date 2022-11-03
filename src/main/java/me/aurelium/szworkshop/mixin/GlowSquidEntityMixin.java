eb1349715034f145cf97f3dea4a2bb05dd3f9dea
AutumnAurelium <AutumnAurelium@users.noreply.github.com>
2022-11-02 20:39:06 -0400

    optimize imports

--- src/main/java/me/aurelium/szworkshop/mixin/GlowSquidEntityMixin.java
+++ src/main/java/me/aurelium/szworkshop/mixin/GlowSquidEntityMixin.java
@@ -7,7 +7,6 @@ import net.minecraft.entity.passive.GlowSquidEntity;
 import net.minecraft.entity.passive.SquidEntity;
 import net.minecraft.entity.player.PlayerEntity;
 import net.minecraft.item.ItemStack;
-import net.minecraft.item.Items;
 import net.minecraft.nbt.NbtCompound;
 import net.minecraft.sound.SoundEvent;
 import net.minecraft.sound.SoundEvents;
@@ -15,10 +14,6 @@ import net.minecraft.util.ActionResult;
 import net.minecraft.util.Hand;
 import net.minecraft.world.World;
 import org.spongepowered.asm.mixin.Mixin;
-import org.spongepowered.asm.mixin.Overwrite;
-import org.spongepowered.asm.mixin.injection.At;
-import org.spongepowered.asm.mixin.injection.Inject;
-import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
 
 @Mixin(GlowSquidEntity.class)
 public class GlowSquidEntityMixin extends SquidEntity implements Bucketable {
